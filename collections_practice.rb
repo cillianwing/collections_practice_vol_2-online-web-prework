12a43ce6ab7ca0c5d05e274ab887eb17f0855c0f
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-06 08:24:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,86 +1,72 @@
-# your code goes here
-def begins_with_r(r_array)
-  r_array.all? do |sub_array|
-    sub_array[0] == "r"
+require "pry"
+
+def begins_with_r(tools_array)
+  tools_array.each do |tool|
+    if tool[0] != "r"
+      return FALSE
+    end
   end
+  TRUE
 end
 
-def contain_a(a_array)
+def contain_a(argument)
   new_array = []
-  a_array.each do |sub_array|
-    if sub_array.include?("a")
-      new_array << sub_array
+  argument.each do |element|
+    if element.include?("a")
+      new_array << element
     end
   end
   new_array
 end
 
-def first_wa(wa_array)
-  wa_array.each do |sub_array|
-    if sub_array[0] + sub_array[1] == "wa"
-      return sub_array
+def first_wa(argument)
+  argument.each do |element|
+    string = element.to_s 
+    if string.start_with?("wa")
+      return string 
     end
   end
 end
 
 def remove_non_strings(array)
   new_array = []
-  array.each do |sub_array|
-    if sub_array.is_a?(String) 
-      new_array << sub_array
+  array.each do |element|
+    if element.is_a?(String)
+      new_array << element
     end
   end
   new_array
 end
 
-def count_elements(collection)
-  collection.group_by(&:itself).map do |key, value|
-    key.merge({:count => value.length})
-  end
-end
-
-def merge_data(keys_array,data_array)
-  data_array.flat_map do |person|
-    person.collect do |name, data|
-      keys_array.each do |first_name|
-        if first_name[:first_name] == name
-          person[name][:first_name] = name
-        end
-      end
-      person[name]
-    end
-  end
-end
-
-def find_cool(cool)
+def count_elements(array)
   new_array = []
-  cool.each do |hash|
-    hash.each do |attribute_key, attribute_value|
-      if attribute_value == "cool"
-        new_array << hash
+  array.each do |name_hash|
+    if new_array.length == 0
+      inner_hash = {}
+      temp_name = name_hash[:name]
+      inner_hash[:name] = temp_name
+      inner_hash[:count] = 1 
+      new_array << inner_hash
+    else
+      new_array.each do |person|
+        inner_hash2 = {}
+        if person.has_value?(name_hash[:name])
+          person[:count] += 1
+        elsif person.has_value?(name_hash[:name]) == false
+          inner_hash2[:name] = name_hash[:name]
+          inner_hash2[:count] = 0 
+          new_array << inner_hash2
+        end
       end
     end
   end
   new_array
 end
 
-def organize_schools(schools)
-  school_hash = {}
-  locations = schools.values.uniq!
-  locations.each do |location_hash|
-    location_hash.each do |key, value|
-      school_hash[value] = []
-    end
-  end
-
-  schools.each do |school, school_location_hash|
-    school_location_hash.each do |location_key, location_value|
-      school_hash.each do |location, array|
-        if location_value == location
-          school_hash[location] << school
-        end
-      end
-    end
-  end
-  school_hash
+def merge_data(data1, data2)
+  
 end
+
+
+
+
